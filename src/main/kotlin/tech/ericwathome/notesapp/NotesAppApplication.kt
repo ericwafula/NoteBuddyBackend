445c2182924f700d7e39db1c0702382ab1bafece
ericwafula <ericwathome007@gmail.com>
2022-08-16 12:23:49 +0300

    initial commit

---
+++ src/main/kotlin/tech/ericwathome/notesapp/NotesAppApplication.kt
@@ -0,0 +1,11 @@
+package tech.ericwathome.notesapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotesAppApplication
+
+fun main(args: Array<String>) {
+    runApplication<NotesAppApplication>(*args)
+}
